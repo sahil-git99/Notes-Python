fc0e1384534a13ad2645df486f6752d24aa1248c
sahil-git99 <sahil.md.work@gmail.com>
2024-12-11 17:30:07 +0530

    added Variable Scope

---
+++ variable_scope_vs_name_equals_main/test.py
@@ -0,0 +1,8 @@
+def hello_world(name):
+    return f"Hello Mr. {name_of_person}"
+
+
+
+if __name__ == "__main__":
+    name_of_person = "Sahil"
+    print(hello_world(name_of_person))
---
+++ variable_scope_vs_name_equals_main/test2.py
@@ -0,0 +1,5 @@
+from test import * 
+
+
+name_of_person = "Michael"
+print(hello_world(name_of_person))
